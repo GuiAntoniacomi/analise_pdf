47db6be8422a245e0d89dfd46ae4c731767f62ae
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-04-05 18:01:30 -0300

    changes

---
+++ Zoneamento/zfr.py
@@ -0,0 +1,299 @@
+zfr = {
+    'Usos Permitidos Habitacionais': {
+        'Habitação Unifamiliar': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 2,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.50,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': None,
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Uma habitação unifamiliar por lote.",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Habitação Coletiva': {
+            'COEFICIENTE DE APROVEITAMENTO': 2,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.50,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Nos terrenos com testada para a rua Treze de Maio em edificações destinadas ao uso misto envolvendo habitação coletiva e estacionamento comercial (ou edifício garagem) são admitidos os seguintes parâmetros: coeficiente de aproveitamento máximo 3 (três); taxa de ocupação de até 100% (cem por",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Habitação Institucional': {
+            'COEFICIENTE DE APROVEITAMENTO': 2,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.50,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Nos terrenos com testada para a rua Treze de Maio em edificações destinadas ao uso misto envolvendo habitação coletiva e estacionamento comercial (ou edifício garagem) são admitidos os seguintes parâmetros: coeficiente de aproveitamento máximo 3 (três); taxa de ocupação de até 100% (cem por",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Habitação Transitória 1': {
+            'COEFICIENTE DE APROVEITAMENTO': 2,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.50,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Nos terrenos com testada para a rua Treze de Maio em edificações destinadas ao uso misto envolvendo habitação coletiva e estacionamento comercial (ou edifício garagem) são admitidos os seguintes parâmetros: coeficiente de aproveitamento máximo 3 (três); taxa de ocupação de até 100% (cem por",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+    },
+    'Usos Não Habitacionais Permitidos': {
+        'Comunitário 1': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Comunitário 2 - Lazer, Cultura': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Com exceção de canchas desportivas.",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Comunitário 2 - Culto Religioso': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Comércio e Serviço Vicinal, de Bairro e Setorial': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Com exceção de comércio de veículos em geral, oficinas de reparação de veículos em geral, serv-car e borracharias",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Estacionamento Comercial e Edifício Garagem': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Obrigatório comércio e serviço vicinal ou de bairro no térreo com acesso direto e abertura independente para via pública, no pavimento térreo da edificação ocupando no mínimo 50% (cinquenta por cento) da testada",
+                " Nos terrenos com testada para a rua Treze de Maio em edificações destinadas ao uso misto envolvendo habitação coletiva e estacionamento comercial (ou edifício garagem) são admitidos os seguintes parâmetros: coeficiente de aproveitamento máximo 3 (três); taxa de ocupação de até 100% (cem por cento) nos dois primeiros pavimentos e 50% (cinquenta por cento) nos demais; altura máxima de até 6 (seis) pavimentos; recuo facultado no alinhamento; afastamento das divisas facultado; taxa de permeabilidade atendendo a legislação específica",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Indústria Tipo 1': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Obrigatório comércio e serviço vicinal ou de bairro no térreo com acesso direto e abertura independente para via pública, no pavimento térreo da edificação ocupando no mínimo 50% (cinquenta por cento) da testada",
+                " Nos terrenos com testada para a rua Treze de Maio em edificações destinadas ao uso misto envolvendo habitação coletiva e estacionamento comercial (ou edifício garagem) são admitidos os seguintes parâmetros: coeficiente de aproveitamento máximo 3 (três); taxa de ocupação de até 100% (cem por cento) nos dois primeiros pavimentos e 50% (cinquenta por cento) nos demais; altura máxima de até 6 (seis) pavimentos; recuo facultado no alinhamento; afastamento das divisas facultado; taxa de permeabilidade atendendo a legislação específica",
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+    },
+    'Usos Não Habitacionais Permissíveis': {
+        'Comunitário 2 e 3 - Ensino': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        },
+        'Supermercados e Centros Comerciais': {
+            'COEFICIENTE DE APROVEITAMENTO': 1,
+            'ALTURA (pavimentos)': 6,
+            'PORTE (m²)': None,
+            'TAXA DE OCUPAÇÃO MÁXIMA': 0.5,
+            'RECUO (m)': 5,
+            'TAXA DE PERMEABILIDADE MÍNIMA': 0.25,
+            'AFASTAMENTO DAS DIVISAS (m)': [
+                {
+                'condicao': 'Até 2 pavimentos',
+                'regra': 'Facultado'
+                },
+                {
+                'condicao': 'Acima de 2 pavimentos',
+                'regra': 'H/6, contado a partir do térreo',
+                'minimo': 2.5
+                 }
+                ],
+            'LOTE PADRÃO (testada x área)': '15x450',
+            'OBSERVACOES': [
+                "Recuo: A critério do Conselho Municipal do Urbanismo-CMU poderá ser admitido alinhamento predial de acordo com a característica da quadra, quando mais de 70% (setenta por cento) das edificações da referida testada da quadra estiverem no alinhamento.",
+                "Taxa de permeabilidade: Atender regulamentação específica."
+            ]
+        }
+    }
+}
+
+def get_zfr_data():
+    return zfr
